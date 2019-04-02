e538c99d651062a38f0e900f198bcbf54deb1e06
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2019-04-03 00:23:50 +0300

    Implemented db connection in python

--- db.py
+++ db.py
@@ -1,2 +1,11 @@
+from pymongo import MongoClient
+
+db_username = "devs"
+db_password = "devs"
+
+client = MongoClient("mongodb://{}:{}@localhost/tasks_db".format(db_username, db_password))
+tasks_db = client["tasks_db"] # This is the database
+tasks = tasks_db["tasks"] # This is a collection (table) in the database
+
 def test():
     return "Test works!"
