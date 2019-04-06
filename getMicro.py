a577cd2893f1c6bf8ed2c2e49cc22d0ba9662896
Teo Dragoi <teodorfdragoi@gmail.com>
2019-04-06 14:34:36 +0300

    test2

--- getMicro.py
+++ getMicro.py
@@ -4,12 +4,12 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 
-client = MongoClient('vvtsoft.ddns.net', '5120')
+client = MongoClient('vvtsoft.ddns.net', 5120)
 
 db = client['db']
 
 tasks = db.tasks
 
-task_cursor = tasks.find( {"_id" : ObjectId("5ca3d06afc79d3e5d41fc0de") } )
+getAll_cursor = tasks.find( {}, {'name' : 1, '_id' : 0} )
 
 list(task_cursor)
