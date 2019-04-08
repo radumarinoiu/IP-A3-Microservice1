4194662f8adec87f3d7be6206eafb5fb43717f29
teodragoi <teodorfdragoi@gmail.com>
2019-04-08 16:46:09 +0000

    scuze

--- db.py
+++ db.py
@@ -3,13 +3,19 @@ import datetime
 import json
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client['tasks_db']
+
+db_username = "devs"
+db_password = "devs"
+
+client = MongoClient("mongodb://{}:{}@localhost/tasks_db".format(db_username, db_password)
+
+tasks_db = client['tasks_db']
+tasks = tasks_db["tasks"]
 
 def post_task(task)
 
 	post = {'nume': task["nume"], 'creare': task["creare"], 'expirare': task["expirare"]}
-	posts = db.tasks
+	posts = tasks
 	post_id = posts.insert_one(post).inserted_id
 	return post_id
 
