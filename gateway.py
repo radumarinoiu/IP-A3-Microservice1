17dc1b67a0a369b589114a3ce9554fe7a3c5ae61
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2019-04-28 00:34:32 +0300

    Bug fix #2 for response streaming on all routes

--- gateway.py
+++ gateway.py
@@ -23,22 +23,22 @@ def get_all_tasks():
 
 @app.route("/tasks/<task_id>", methods = ["GET"])
 def get_task(task_id):
-    resp = requests.get(TASKS_MS_ADDRESS_PORT + "/" + task_id)
+    resp = requests.get(TASKS_MS_ADDRESS_PORT + "/" + task_id, stream=True)
     return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/tasks", methods = ["POST"])
 def add_task():
-    resp = requests.post(TASKS_MS_ADDRESS_PORT, json=request.json)
+    resp = requests.post(TASKS_MS_ADDRESS_PORT, json=request.json, stream=True)
     return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/tasks/<task_id>", methods = ["PUT"])
 def update_task(task_id):
-    resp = requests.put(TASKS_MS_ADDRESS_PORT + "/" + task_id, json=request.json)
+    resp = requests.put(TASKS_MS_ADDRESS_PORT + "/" + task_id, json=request.json, stream=True)
     return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/tasks/<task_id>", methods = ["DELETE"])
 def delete_task(task_id):
-    resp = requests.delete(TASKS_MS_ADDRESS_PORT + "/" + task_id)
+    resp = requests.delete(TASKS_MS_ADDRESS_PORT + "/" + task_id, stream=True)
     return resp.raw.read(), resp.status_code, resp.headers.items()
 
 
@@ -46,28 +46,27 @@ def delete_task(task_id):
 
 @app.route("/assigner", methods = ["GET"])
 def get_all_assignments():
-    resp = requests.get(ASSIGNER_MS_ADDRESS_PORT)
+    resp = requests.get(ASSIGNER_MS_ADDRESS_PORT, stream=True)
     return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/assigner/<assignment_id>", methods = ["GET"])
 def get_assignment(assignment_id):
-    resp = requests.get(ASSIGNER_MS_ADDRESS_PORT + "/" + assignment_id)
+    resp = requests.get(ASSIGNER_MS_ADDRESS_PORT + "/" + assignment_id, stream=True)
     return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/assigner", methods = ["POST"])
 def add_assignment():
-    print(request.json)
-    resp = requests.post(ASSIGNER_MS_ADDRESS_PORT, json=request.json)
+    resp = requests.post(ASSIGNER_MS_ADDRESS_PORT, json=request.json, stream=True)
     return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/assigner/<assignment_id>", methods = ["PUT"])
 def update_assignment(assignment_id):
-    resp = requests.put(ASSIGNER_MS_ADDRESS_PORT + "/" + assignment_id, json=request.json)
+    resp = requests.put(ASSIGNER_MS_ADDRESS_PORT + "/" + assignment_id, json=request.json, stream=True)
     return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/assigner/<assignment_id>", methods = ["DELETE"])
 def delete_assignment(task_id):
-    resp = requests.delete(ASSIGNER_MS_ADDRESS_PORT + "/" + task_id)
+    resp = requests.delete(ASSIGNER_MS_ADDRESS_PORT + "/" + task_id, stream=True)
     return resp.raw.read(), resp.status_code, resp.headers.items()
 
 
