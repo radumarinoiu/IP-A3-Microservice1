84ddfd8e8f74dfb64b890d1059901c6cf90ea228
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2019-04-28 00:24:58 +0300

    Streaming the response components in binary mode for all routes

--- gateway.py
+++ gateway.py
@@ -23,42 +23,51 @@ def get_all_tasks():
 
 @app.route("/tasks/<task_id>", methods = ["GET"])
 def get_task(task_id):
-    return requests.get(TASKS_MS_ADDRESS_PORT + "/" + task_id)
+    resp = requests.get(TASKS_MS_ADDRESS_PORT + "/" + task_id)
+    return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/tasks", methods = ["POST"])
 def add_task():
-    return requests.post(TASKS_MS_ADDRESS_PORT, request.json)
+    resp = requests.post(TASKS_MS_ADDRESS_PORT, request.json)
+    return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/tasks/<task_id>", methods = ["PUT"])
 def update_task(task_id):
-    return requests.put(TASKS_MS_ADDRESS_PORT + "/" + task_id, request.json)
+    resp = requests.put(TASKS_MS_ADDRESS_PORT + "/" + task_id, request.json)
+    return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/tasks/<task_id>", methods = ["DELETE"])
 def delete_task(task_id):
-    return requests.delete(TASKS_MS_ADDRESS_PORT + "/" + task_id)
+    resp = requests.delete(TASKS_MS_ADDRESS_PORT + "/" + task_id)
+    return resp.raw.read(), resp.status_code, resp.headers.items()
 
 
 # Assigner Routes
 
 @app.route("/assigner", methods = ["GET"])
 def get_all_assignments():
-    return requests.get(TASKS_MS_ADDRESS_PORT)
+    resp = requests.get(TASKS_MS_ADDRESS_PORT)
+    return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/assigner/<assignment_id>", methods = ["GET"])
 def get_assignment(assignment_id):
-    return requests.get(TASKS_MS_ADDRESS_PORT + "/" + assignment_id)
+    resp = requests.get(TASKS_MS_ADDRESS_PORT + "/" + assignment_id)
+    return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/assigner", methods = ["POST"])
 def add_assignment():
-    return requests.post(TASKS_MS_ADDRESS_PORT, request.json)
+    resp = requests.post(TASKS_MS_ADDRESS_PORT, request.json)
+    return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/assigner/<assignment_id>", methods = ["PUT"])
 def update_assignment(assignment_id):
-    return requests.put(TASKS_MS_ADDRESS_PORT + "/" + assignment_id, request.json)
+    resp = requests.put(TASKS_MS_ADDRESS_PORT + "/" + assignment_id, request.json)
+    return resp.raw.read(), resp.status_code, resp.headers.items()
 
 @app.route("/assigner/<assignment_id>", methods = ["DELETE"])
 def delete_assignment(task_id):
-    return requests.delete(TASKS_MS_ADDRESS_PORT + "/" + task_id)
+    resp = requests.delete(TASKS_MS_ADDRESS_PORT + "/" + task_id)
+    return resp.raw.read(), resp.status_code, resp.headers.items()
 
 
 # Run App
