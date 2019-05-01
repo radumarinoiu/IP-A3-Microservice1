ab1a535b42ba3e047800c53e8d181d8d7a821da9
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2019-05-01 13:21:19 +0300

    Removed duplicate code.

--- tasks/db.py
+++ tasks/db.py
@@ -35,29 +35,6 @@ def check_status(task):
     return False
 
 
-def check_task_fields(task):
-    task_fields = ["name", "category", "department", "creator", "description", "priority", "status", "deadline", "timestamp"]
-    task_field_lists = {
-        "participants": ["_id"],
-        "sub-tasks": ["name", "description", "deadline", "status", "priority"],
-        # "dependencies": ["task-id", "deadline", "status", "priority"],
-        # "reverse-dependecies": ["task-id", "deadline", "status", "priority"],
-        "dependencies": ["_id"],
-        "reverse-dependecies": ["_id"],
-        "commits": ["commit_url", "username", "changes", "timestamp"]
-    }
-
-    for field in task_fields:
-        if field not in task:
-            return 400
-
-    for task_field in task_field_lists:
-        if task_field in task:
-            for task_field_field in task_field_lists[task_field]:
-                if task_field_field not in task[task_field]:
-                    return 400
-
-
 def post_task(task):
     task_fields = ["name", "category", "department", "creator", 
     "description", "priority", "status", "deadline", "timestamp"]
