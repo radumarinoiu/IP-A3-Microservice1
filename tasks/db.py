26dd6139e282a87653b55f06ec445c047435832f
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2019-05-01 14:42:42 +0300

    Testing return with jsonify

--- tasks/db.py
+++ tasks/db.py
@@ -4,8 +4,6 @@ import json
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from flask import jsonify
-from flask import Response
-from flask import make_response
 
 db_username = "devs"
 db_password = "devs"
@@ -19,42 +17,42 @@ def getAll():
     tasks_list = list(coll.find({}))
     for task in tasks_list:
         task["_id"] = str(task["_id"])
-    return tasks_list, 200
+    return jsonify(tasks_list), 200
 
 
 def getById(task_id):
     task = coll.find_one({"_id" : ObjectId(task_id)})
     if not task:
-        return {}, 404
+        return jsonify({}), 404
     else:
         task["_id"] = str(task["_id"])
-        return task, 200
+        return jsonify(task), 200
 
 
 def post_task(task):
     if not task:
-        return {"error": "Empty request."}, 400
+        return jsonify({"error": "Empty request."}), 400
     
     # Check task's fields
     message, result = check_task_fields(task)
     if not result:
-        return message, 400
+        return jsonify(message), 400
 
     post_id = coll.insert_one(task).inserted_id
     task["_id"] = str(post_id)
-    return task, 201
+    return jsonify(task), 201
 
 
 def put_task(task):
     if not task:
-        return {"error": "Empty request."}, 400
+        return jsonify({"error": "Empty request."}), 400
     if "_id" not in task:
-        return {"error": "Put request missing _id"}, 400
+        return jsonify({"error": "Put request missing _id"}), 400
     
     # Check task's fields
     message, result = check_task_fields(task)
     if not result:
-        return message, 400
+        return jsonify(message), 400
 
     task["_id"] = ObjectId(task["_id"])
     coll.update(
@@ -62,14 +60,14 @@ def put_task(task):
         {"$set": task},
         upsert=False)
     task["_id"] = str(task["_id"])
-    return task, 200
+    return jsonify(task), 200
 
 
 def deleteById(id):
     deletedId = coll.find_one({"_id" : ObjectId(id)})
     deletedId["_id"] = str(deletedId["_id"])
     coll.remove({"_id" : ObjectId(id)})
-    return {}, 200
+    return jsonify({}), 200
 
 
 def check_task_fields(task):
@@ -87,12 +85,12 @@ def check_task_fields(task):
 
     for field in task_fields:
         if field not in task:
-            return {"error": "Field {} is required but not present.".format(field)}, False
+            return jsonify({"error": "Field {} is required but not present.".format(field)}), False
 
     for task_field in task_field_lists:
         if task_field in task:
             for task_field_field in task_field_lists[task_field]:
                 if task_field_field not in task[task_field]:
-                    return {"error": "Field {} from {} is required but not present.".format(
-                        task_field_field, task_field)}, False
-    return {}, True
+                    return jsonify({"error": "Field {} from {} is required but not present.".format(
+                        task_field_field, task_field)}), False
+    return jsonify({}), True
