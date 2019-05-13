2fd5af620928f1f79bfc05a97b3b1b8e449ddc70
Teo Dragoi <teodorfdragoi@gmail.com>
2019-05-13 15:05:48 +0300

    Added check if available for update

---
+++ tasks/checkScript.py
@@ -0,0 +1,25 @@
+import db
+
+import json
+import pymongo
+import logging
+import traceback
+
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from bson.errors import InvalidId as InvalidIdException
+from flask import jsonify
+from jsonschema import validate
+
+with open('object.json') as json_file:
+    json_data = json.load(json_file)
+    for data in json_data['sub-tasks']:
+        isCompleted = 0
+        subTask = json.load(db.getById(data['id']))
+        if(subTask['status'] == 'complete'):
+            isCompleted = 1
+        else:
+            isCompleted = 0
+            break
+    if(isCompleted == 1):
+        json_data['status'] = 'complete'
