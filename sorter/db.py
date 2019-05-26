19ea2f59d985a5e0d3b82c08092325fdfdfd0b6d
Andrei <scoala.andrei@yahoo.com>
2019-05-26 23:45:29 +0300

    Update sorter

--- sorter/db.py
+++ sorter/db.py
@@ -1,40 +1,52 @@
 from flask import jsonify
 
-task_diff_for_period = {
-    "hard": "tired",
-    "easy": "relaxed"
+task_priority_index = {
+    "Low": 1,
+    "Medium": 2,
+    "High": 3
 }
 
 
 def sortTasks(data):
     preferences = data["preferences"]
     tasks = data["tasks"]
-    sortedTasks = {
-        "morning": [],
-        "afternoon": [],
-        "evening": []
+    sorted_filtered_tasks = {
+        "Low": [],
+        "Medium": [],
+        "High": []
     }
 
-    sortedPreferences = {
-        "relaxed": [],
-        "tired": []
+    final_periods = {
+        "morning": None,
+        "afternoon": None,
+        "evening": None
     }
+
+    final_priorities = {
+        "Low": [],
+        "Medium": [],
+        "High": []
+    }
+
+    for priority in sorted_filtered_tasks:
+        new_list = []
+        for task in tasks:
+            if task_priority_index[task["priority"]] <= task_priority_index[priority]:
+                new_list.append(task)
+        deadline_sorted_list = sorted(new_list, key=lambda k: k['deadline'])
+        sorted_filtered_tasks[priority] = sorted(
+            deadline_sorted_list,
+            key=lambda k: task_priority_index[k['priority']],
+            reverse=True
+        )
 
     for pref in preferences:
-        if preferences[pref] in sortedPreferences:
-            sortedPreferences[preferences[pref]].append(pref)
+        if preferences[pref] in final_priorities:
+            final_priorities[preferences[pref]].append(pref)
 
-    for pref in sortedPreferences:
-        temp_tasks = []
-        if sortedPreferences[pref]:
-            for task in tasks:
-                if task["priority"] in task_diff_for_period and \
-                        task_diff_for_period[task["priority"]] == pref:
-                    temp_tasks.append(task)
-            while temp_tasks:
-                for period in sortedPreferences[pref]:
-                    if not temp_tasks:
-                        break
-                    sortedTasks[period].append(temp_tasks.pop())
+    for priority in final_priorities:
+        for period in final_priorities[priority]:
+            if sorted_filtered_tasks[priority]:
+                final_periods[period] = sorted_filtered_tasks[priority].pop(0)
 
-    return jsonify(sortedTasks), 200
+    return jsonify(final_periods), 200
